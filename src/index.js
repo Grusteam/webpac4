4a56dc460929b36abd7252f9a702148ec41853c5
Serge Uniquite <sergeuniquite@gmail.com>
2019-01-15 16:33:33 +0300

    tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+/* styles */
 import "./styles/styles.css";
-import _ from 'lodash';
 // import "./styles/test.css";
+
+/* lodash - импортировать !МОДУЛЯМИ */
+import _join from 'lodash/join';
+// import _forEach from 'lodash/forEach';
 
 function component() {
 	const element = document.createElement('div');
 
 	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = _.join(['Hello', 'world'], ' ');
+	element.innerHTML = _join(['Hello', 'from JS',], ' ');
 	element.classList.add('hello');
 
    element.style.backgroundImage = 'url(./public/images/dog.png)';
@@ -21,3 +25,11 @@ const node = document.body.appendChild(component());
 // const arr = [1, 2, 3];
 // const iAmJavascriptES6 = () => console.log(...arr);
 // window.iAmJavascriptES6 = iAmJavascriptES6;
+
+/* rest / spread> */
+/* const
+	arr = ['x', 'y', 'z'],
+	f_arr = (...all) => all;
+
+console.log('...f_arr(...arr)', ...f_arr(...arr)); */
+/* <rest / spread */
